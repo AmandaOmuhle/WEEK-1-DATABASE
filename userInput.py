25a98f113e5086d4acde256225a9ade81cf38aac
Amanda Mthembu <132444890+AmandaOmuhle@users.noreply.github.com>
2025-03-18 03:51:28 +0200

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,8 @@
+A = int(input("Enter the value of A:"))
+
+B = int(input("Enter the value of B:"))
+
+print("Sum=", A+B)
+print("Subtraction=",A-B)
+print("Multiplication=",A*B)
+print("Division=", A/B)
